15dc1e23e9d71f11c19b2e778097579f31de26d8
Unknown <calangodeguerra@outlook.com>
2019-06-03 18:23:21 -0300

    Minor update

--- serialTeste.py
+++ serialTeste.py
@@ -1,7 +1,13 @@
 import serial
 
 
-def set_config(): 
+def set_config(command='', param='', option=''):
+    #Para o cabeçalho dessa função:
+    #command -> Código do comando a ser enviado
+    #param - > Codigo das opções. Ex.: Numero do botão
+    #opções -> Informações auxiliares em hex. Ex.: Tempo em segundos (hexadecimal)
+
+    #Configuração da comunicação:
     # Primeiro parametro: Porta onde tá a placa que vai ser lida
     # Segundo parâmetro: Velocidade de transferência de dados (em bits/s)
     # Terceiro parâmetro: Tempo de timeout em segundos
@@ -9,30 +15,22 @@ def set_config():
 
     token_ACK='99' #Token para inicio de comunicação
     token_FIN='ff' #Token para fim de comunicação
-    command = 'a1b2c3' #Comando que se deseja enviar
+    # command = 'a1b2c3' #Comando que se deseja enviar
 
 
-    msgToSent = command+token_FIN #Mensagem em hex, sem o prefixo 0x, para se enviar
+    #Mensagem em hex, sem o prefixo 0x, para se enviar
+    msgToSent = token_ACK+command+option+param+token_FIN 
 
-    msgToSent = bytes.fromhex(msgToSent) #Codificação da mensagem em bytes
+    #Codificação da mensagem em bytes
+    msgToSent = bytes.fromhex(msgToSent) 
 
+    print(msgToSent)  # Pequena prévia da mensagem a ser enviada
+    return_set = ser.write((msgToSent))  # Pega o numero de bytes enviado
+    print("Bytes sent: ", return_set)  # Exibe o numero de bytes enviado
+
+
+ 
     msgReceived=''
-
-
-
-    print("Byte 1? ", msgToSent[0])
-
-    print(msgToSent)
-
-
-
-
-    retrono = ser.write((msgToSent))
-
-    print("Retorno: ", retrono)
-
-    # msgReceived = ser.readline()
-
     msgReceived = ser.read_until(bytes.fromhex(token_FIN))
     ser.close()
     msgReceived = msgReceived.decode('ISO-8859-1')
@@ -62,22 +60,25 @@ def get_value(option=''):
     print("Bytes sent: ", return_get) #Exibe o numero de bytes enviado
 
 
-    msgReceived = ''
+    # msgReceived = ''
     # msgReceived = ser.readline()
-    
+
     msgReceived = ser.read_until(bytes.fromhex(token_FIN))
     ser.close()
-    msgReceived = msgReceived.decode('ISO-8859-1')
 
+    #Amostra o que foi recebido no terminal
+    #Tem que usar esse codec aí porque o UTF-8 num aguenta não
+    # msgReceived = msgReceived.decode('ISO-8859-1')
+    
+    #Mensagem recebida em bytes
     print("Mensagem recebida: ", msgReceived)
     # print(ser.read_until(bytes.fromhex(token)))
 
 
-
-
 def main():
   print("Hello World!")
-  communication()
+  get_value('b2')
+  
 
 
 if __name__ == "__main__":
