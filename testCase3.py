f1ec5c9eb06b99a59fe993df9df28e82e21cb9f7
Otto Pinto <calangodeguerra@outlook.com>
2019-06-12 16:59:47 -0300

    Case test 3 almost complete

--- testCase3.py
+++ testCase3.py
@@ -4,12 +4,55 @@ from get_panel import *
 
 
 
+def validateLED(ledInfo, ledToken):
+    ###### Validação do teste ######
+
+     ## Validação do LED ##
+
+    auxLedValidation = ledInfo
+
+    if(auxLedValidation == ledToken):
+        print("Test OK. Correct LED turned on")
+    else:
+        print("Test failed. Incorrect LED turned on")
+
+    
+
+
+def validateBuzzer(buzzerInfo):
+    ## Validação do buzzer ##
+
+    auxBuzzerValidation = buzzerInfo
+
+    #Tempo de buzzer ligado depende da situação
+
+    ##Validação do tempo em alto do buzzer para o caso atual
+    buzzHighTime = 100  # Tempo em milissegundos
+    # Não sei se a placa me envia os tempos nessa ordem não
+    buzzRelativeTime = [10, 20, 40, 60]
+
+    for i in range(len(buzzerInfo)):
+
+        if(auxBuzzerValidation[i][0] == buzzHighTime):
+            print("Buzzer high time complies the specification for this case.")
+        else:
+            print("Buzzer high time does not comply the specification")
+
+        ##Validação do tempo relativo do buzzer para o caso atual
+
+        if(auxBuzzerValidation[i][1] == buzzRelativeTime):
+            print("Relative time complies the specification")
+
+        else:
+            print("Relative time does not comply the specifitation")
+
+
 def sceneOne():
     
     '''
     Todos os comandos abaixo descritos devem ser escritos 
     como hexadecimais, mesmo se for um número. Por exemplo,
-    a variável timeUp recebe valores inteiros arbritários. 
+    a variável ON_OFF_TIME recebe valores inteiros arbritários. 
     Esses inteiros devem ser convertidos pra hexadecimal
     de dois digitos e depois atribuídos a variável. 
     Ex.: 10 (inteiro) -> 0A (hexadecimal)  
@@ -20,16 +63,16 @@ def sceneOne():
     buttonPower = '12'
     buzzerInfo = []
     ledInfo = []
-    
+    ON_OFF_TIME = '01'  # Valor inteiro '10'
 
     #Configurar qual botão vai ser apertado
-    #######################   Cenario 1   ############################# 
+    #########################################   Cenario 1   ###############################################
 
     ####################### Perfil de 10s #############################
     
-    timeUp = '0A' # Valor inteiro '10'    
+     
     ### Inicia o perfil de cura. 
-    returnSet = set_config(command, buttonPower, timeUp)    
+    returnSet = set_config(command, buttonPower, ON_OFF_TIME)    
     
     if (returnSet == bytes.fromhex('99' + command + 'FF')):
 
@@ -39,30 +82,35 @@ def sceneOne():
 
         print("Button configured")
 
-        time.sleep(0.2)  # Dorme por X segundos
+       
         ledInfo = ledInfo + getPanel()
-
+        time.sleep(10)  # Dorme por X segundos
         buzzerInfo = buzzerInfo + getBuzzer()
 
 
     else: 
         print("Error on buttonPower configuration")
-    
 
+
+    
+    
+    validateLED(ledInfo[0], '01')
+    validateBuzzer(buzzerInfo[0])
+
+    
+    #########################################################################   
 
    ####################### Perfil de 20s #############################
 
-    timeUp = '0A'  # Para simular um aperto do botão durante 10s
-
     ### Configura o perfil de cura ###
-    returnSet = set_config(command, buttonArrow, timeUp)
+    returnSet = set_config(command, buttonArrow, ON_OFF_TIME)
     
 
     if (returnSet == bytes.fromhex('99' + command + 'FF')):
          # Se a configuração do botão seta der certo, inicia 
          # o perfil de cura   
         print("Button configured")
-        auxReturn = set_config(command, buttonPower, timeUp)
+        auxReturn = set_config(command, buttonPower, ON_OFF_TIME)
         # Esse if aqui verifica seo comando pra iniciar
         # o perfil de cura foi pressionado com sucesso.
         # Se não tiver sido, retorna mensagem de erro.
@@ -75,7 +123,7 @@ def sceneOne():
 
             ledInfo = ledInfo + getPanel()
 
-            time.sleep(200)
+            time.sleep(20)
 
             buzzerInfo = buzzerInfo + getBuzzer()
 
@@ -86,19 +134,27 @@ def sceneOne():
 
     else:
         print("Error on buttonArrow configuration")
+
+
+    validateLED(ledInfo[1], '02')
+    validateBuzzer(buzzerInfo[1])
+
+
+
+
+ #########################################################################
 
  ####################### Perfil de 40s #############################
 
-    timeUp = '0A'  # Para simular um aperto do botão durante 10s
-
+   
     ### Configura o perfil de cura ###
-    returnSet = set_config(command, buttonArrow, timeUp)
+    returnSet = set_config(command, buttonArrow, ON_OFF_TIME)
 
     if (returnSet == bytes.fromhex('99' + command + 'FF')):
          # Se a configuração do botão seta der certo, inicia
          # o perfil de cura
         print("Button configured")
-        auxReturn = set_config(command, buttonPower, timeUp)
+        auxReturn = set_config(command, buttonPower, ON_OFF_TIME)
         # Esse if aqui verifica seo comando pra iniciar
         # o perfil de cura foi pressionado com sucesso.
         # Se não tiver sido, retorna mensagem de erro.
@@ -111,7 +167,7 @@ def sceneOne():
 
             ledInfo = ledInfo + getPanel()
 
-            time.sleep(400)
+            time.sleep(40)
 
             buzzerInfo = buzzerInfo + getBuzzer()
 
@@ -119,20 +175,26 @@ def sceneOne():
             print("Error on buttonPower configuration")
 
     else:
-        print("Error on buttonArrow configuration")
+      print("Error on buttonArrow configuration")
+
+    
+    validateLED(ledInfo[2], '04')
+    validateBuzzer(buzzerInfo[2])
+
+
+
+ #########################################################################
 
  ####################### Perfil de 60s #############################
 
-    timeUp = '0A'  # Para simular um aperto do botão durante 10s
-
     ### Configura o perfil de cura ###
-    returnSet = set_config(command, buttonArrow, timeUp)
+    returnSet = set_config(command, buttonArrow, ON_OFF_TIME)
 
     if (returnSet == bytes.fromhex('99' + command + 'FF')):
          # Se a configuração do botão seta der certo, inicia
          # o perfil de cura
         print("Button configured")
-        auxReturn = set_config(command, buttonPower, timeUp)
+        auxReturn = set_config(command, buttonPower, ON_OFF_TIME)
         # Esse if aqui verifica seo comando pra iniciar
         # o perfil de cura foi pressionado com sucesso.
         # Se não tiver sido, retorna mensagem de erro.
@@ -145,7 +207,7 @@ def sceneOne():
 
             ledInfo = ledInfo + getPanel()
 
-            time.sleep(600)
+            time.sleep(60)
 
             buzzerInfo = buzzerInfo + getBuzzer()
 
@@ -155,6 +217,14 @@ def sceneOne():
     else:
         print("Error on buttonArrow configuration")
 
+
+    validateLED(ledInfo[3], '08')
+    validateBuzzer(buzzerInfo[3])
+
+
+       
+
+ #########################################################################
 
 
 def sceneTwo():
@@ -176,13 +246,13 @@ def sceneTwo():
 
     '''
     
-    timeUp = '0A' # Valor inteiro '10'   
+    ON_OFF_TIME = '01' # Valor inteiro '10'   
     ledInfoBefore = '' 
     ledInfoAfter = ''
     
     ledInfoBefore = getPanel()
 
-    returnSet = set_config(command, buttonArrow, timeUp)  #Pressiona o botão por 10 * 100ms 
+    returnSet = set_config(command, buttonArrow, ON_OFF_TIME)  #Pressiona o botão por 10 * 100ms 
     
     if (returnSet == bytes.fromhex('99' + command + 'FF')):
 
@@ -235,11 +305,18 @@ def sceneTwo():
 
       
  ####################### Botão Power Pressionado < ON_OFF_TIME segundos #############################     
+ # Falta implementar
+   #
 
-    timeUp = '0A'  # Valor inteiro '10'
+
+
+
+####################### Botão Power Pressionado > ON_OFF_TIME segundos #############################
+  
+    ON_OFF_TIME = '05'  # Valor inteiro '5'
 
     # Pressiona o botão por 10 * 100ms
-    returnSet = set_config(command, buttonPower, timeUp)
+    returnSet = set_config(command, buttonPower, ON_OFF_TIME)
 
     if (returnSet == bytes.fromhex('99' + command + 'FF')):
 
@@ -249,9 +326,14 @@ def sceneTwo():
 
         print("Button configured")
 
-        time.sleep(0.2)  # Dorme por X segundos
-
-        ledInfo = ledInfo + getPanel()
+        ledInfo = getPanel()
 
     else:
         print("Error on buttonPower configuration")
+
+
+    if(ledInfo == '03' or ledInfo == '07' or ledInfo == '0f'):
+        print("Battery level test ok.")
+        print("Current Level: ", ledInfo)
+    else:
+        print("Battery level test find an error")
