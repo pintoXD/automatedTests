8815d6d8f53c2b02a0d3fe884784811a36c5f00f
Otto Pinto <calangodeguerra@outlook.com>
2019-07-04 18:48:46 -0300

    minor update

--- sketchs/example.py
+++ sketchs/example.py
@@ -1,24 +1,8 @@
-import time
-cont = 0
-i = 0
-while i < 5:
+a = [(1,2), (3,4)]
 
-    time.sleep(0.005)
 
-    print(i)
-    i = i + 1
-    
-    # if i == 1:
-    #     i = 0
-    
-    cont = cont + 1
 
-    if cont == 15:
-        print(cont)
-        break
-    
-
-print(i)
+print(a[0][0])
 
 
 
