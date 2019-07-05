87d7cbf98418bb03d6850e3846670c5015560ba2
Otto Pinto <calangodeguerra@outlook.com>
2019-07-05 19:31:27 -0300

    minor updates

--- sketchs/example.py
+++ sketchs/example.py
@@ -1,21 +1,37 @@
-# buz = '010000001601000001410100002f74010000309f'
-buz = '01 00000 01 7 01 0000 01 42'
-buz_info = []
+# # buz = '010000001601000001410100002f74010000309f'
+# buz = '01 00000 01 7 01 0000 01 42'
+# buz_info = []
 
-while(len(buz) != 0):
+# while(len(buz) != 0):
 
-     print("Buz inside ", buz)
-     print("Buzinfo inside ", buz_info)
+#      print("Buz inside ", buz)
+#      print("Buzinfo inside ", buz_info)
 
-     buz_info = buz_info + [(int(buz[:2], 16), int(buz[2:10], 16))]
-     buz = buz[10:]
-     #Sugestão de correção se necessário
-     # if (x < 3):
-     #     buz_infoA = buz_info + (int(buz[x], 16), int(buz[x+1], 16))
-       # else:
-       #     buz_infoB = buz_info + (int(buz[x], 16), int(buz[x+1], 16))
+#      buz_info = buz_info + [(int(buz[:2], 16), int(buz[2:10], 16))]
+#      buz = buz[10:]
+#      #Sugestão de correção se necessário
+#      # if (x < 3):
+#      #     buz_infoA = buz_info + (int(buz[x], 16), int(buz[x+1], 16))
+#        # else:
+#        #     buz_infoB = buz_info + (int(buz[x], 16), int(buz[x+1], 16))
 
     
     
-print("Buz outside ", buz)
-print("Buzinfo outside ", buz_info)
+# print("Buz outside ", buz)
+# print("Buzinfo outside ", buz_info)
+cont = 40
+
+with open('teste.txt', 'a') as f:
+    
+    print("############# INIT ##################")
+
+
+    print("Successful tests percentage: ", (cont/50)*100, file=f)
+
+    print("Unsuccessful tests percentage: ", ((50 - cont)/50) * 100, file=f)
+
+    print("############# END ##################")
+
+    # print("Elapsed time: ", time.time() - initialTime, file=f)
+
+f.close()
