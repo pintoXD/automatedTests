d6778a3a08e2f43dc14a58311167e9e4bd974e26
Otto Pinto <calangodeguerra@outlook.com>
2019-07-25 21:50:34 -0300

    minor updates

--- TC3/testCase3.py
+++ TC3/testCase3.py
@@ -107,7 +107,7 @@ def profile(desiredCureProfile):
                 while(auxContTimes < 50):
                     print("Inside second while")
                     set_config(command, buttonPower, pressTime)
-                    time.sleep(0.5)
+                    time.sleep(2)
                     if(getPotLum() > 0):
                         break
                     auxContTimes = auxContTimes + 1
@@ -351,8 +351,8 @@ def profile(desiredCureProfile):
                 while(auxContTimes < 50):
                     print("Inside second while to turn on")
                     set_config(command, buttonPower, pressTime)
-                    time.sleep(0.5)
-                    if(getPotLum > 0):
+                    time.sleep(2)
+                    if(getPotLum() > 0):
                         break
                     auxContTimes = auxContTimes + 1
                     if(auxContTimes > 0):
